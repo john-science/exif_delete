642d0f77f72ed6e6565a4fc9e1c573aa1467447b
John Stilley <thejollysin@gmail.com>
2017-06-15 09:56:15 -0700

    placeholder for run script

---
+++ exif_delete.py
@@ -0,0 +1,33 @@
+"""
+Purpose:
+
+    The `exif_delete.py` script allows you to strip all of the metadata from an image file to protect your privacy.
+
+Flags:
+
+    TBD
+
+Usage:
+
+    TBD
+"""
+
+# standard libraries
+import sys
+# third-party libraries
+import PIL
+
+
+def main():
+    print('ERROR: Coming Soon: This script is currently just a placeholder, that does nothing.')
+
+
+def usage():
+    ''' Print a help menu to the screen, if the user enters a bad command line flag.
+    '''
+    print(__doc__)
+    exit()
+
+
+if __name__ == '__main__':
+    main()
