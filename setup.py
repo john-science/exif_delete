843f78fea44606447e7066d0a222667a482a1f32
theJollySin <john_science@protonmail.com>
2019-07-24 20:50:27 -0400

    #3 reworking install

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     classifiers=[
         "Development Status :: 4 - Beta",
         "Environment :: Console",
-        "License :: OSI Approved :: GPLv3 License",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
